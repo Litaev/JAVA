a4e7547b835df7e9d8a53414507b944164f8038f
Litaev <lexa.228hahy@gmail.com>
2025-02-19 07:25:02 +0300

    laba1

--- src/main/java/com/example/sb/controllers/CarController.java
+++ src/main/java/com/example/sb/controllers/CarController.java
@@ -3,19 +3,21 @@ package com.example.sb.controllers;
 import org.springframework.web.bind.annotation.*;
 import org.apache.commons.text.StringEscapeUtils;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api")
 public class CarController  {
 
     @GetMapping("/cars")
-    public String getQueryCar(@RequestParam("name") String name) {
+    public Map<String, String> getQueryCar(@RequestParam("name") String name) {
         String validatedName = StringEscapeUtils.escapeHtml4(name);
-        return "Car name is " + validatedName;
+        return Map.of("carName", validatedName);
     }
 
     @GetMapping("/cars/{carID}")
-    public String getPathCar(@PathVariable("carID") Integer carID) {
-        return "Car ID is " + carID.toString();
+    public Map<String, Integer> getPathCar(@PathVariable("carID") Integer carID) {
+        return Map.of("carID", carID);
     }
 
 
