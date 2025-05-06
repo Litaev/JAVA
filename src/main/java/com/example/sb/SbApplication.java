163c6d15292fd7518662004d67df5623bdbbd4d7
Litaev <lexa.228hahy@gmail.com>
2025-05-06 03:41:08 +0300

    docker fix

--- src/main/java/com/example/sb/SbApplication.java
+++ src/main/java/com/example/sb/SbApplication.java
@@ -3,17 +3,21 @@ package com.example.sb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * SpringBootApp.
  */
+@RestController
 @SpringBootApplication
 public class SbApplication {
-  /**
-   * main.
-   */
   public static void main(String[] args) {
     SpringApplication.run(SbApplication.class, args);
   }
 
+  @GetMapping("/")
+  public String home() {
+    return "Application is running!";
+  }
 }
